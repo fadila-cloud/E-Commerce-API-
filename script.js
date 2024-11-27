949966cc0782eb38c83dd8007bbdeec71422028a
Ilma Fadilah <ilmafadilah225@gmail.com>
2024-11-27 19:52:14 +0700

    styling produk api publik

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // URL API Fake Store
-const apiUrl ='https://fakestoreapi.com/products'
+const apiUrl = "https://fakestoreapi.com/products";
 
 // Fungsi untuk mengambil dan menampilkan data
 async function fetchAndDisplayProduct() {
   try {
     // Fetch data dari API
     const response = await fetch(apiUrl);
-    if (!response.ok) throw new Error('Gagal mengambil data');
+    if (!response.ok) throw new Error("Gagal mengambil data");
 
     const data = await response.json();
 
     // Dapatkan container untuk menampilkan produk
-    const container = document.getElementById('products-container');
-    container.innerHTML = ''; // membersihkan semua isi di dlm container
+    const container = document.querySelector(".products-container"); // untuk mengambil elemen tunggal
+    container.innerHTML = ""; // membersihkan semua isi di dlm container
 
     // iterasi semua produk dengan forEach
     data.forEach((product) => {
@@ -21,19 +21,21 @@ async function fetchAndDisplayProduct() {
         <img src="${product.image}" alt="${product.title}">
         <h3>${product.title}</h3>
         <p class="description">${product.description}</p>
-        <p class="price">Price: $${product.price}</p>
-        <p class="rating">Rating: ${product.rating.rate} (${product.rating.count} reviews)</p>
+        <div class="pricerate">
+          <p class="price">Price: $${product.price}</p>
+          <p class="rating">Rating: ${product.rating.rate} (${product.rating.count} reviews)</p>
+        </div>
+        <button class="troli-cart">Tambahkan ke Keranjang</button>
       </div>
     `;
 
-    // menambahkan produk ke container
-    container.innerHTML += productHtml;
+      // menambahkan produk ke container
+      container.innerHTML += productHtml;
     });
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   }
 }
 
 // Panggil fungsi untuk fetch dan tampilkan produk
 fetchAndDisplayProduct();
-
